b8be98decf0985e35971c0338f55a0cb2f12b2fc
sebi <gore.sebyx@yahoo.com>
2018-09-13 15:25:01 +0300

    update index

---
+++ frontend/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <page-nav-bar></page-nav-bar>
+    <div class="container">
+      <router-view/>
+    </div>
+  </div>
+</template>
+
+<script>
+    import PageNavBar from "./components/page/nav-bar";
+    export default {
+        components: {PageNavBar}
+    }
+</script>
