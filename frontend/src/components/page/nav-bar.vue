71fadac1a25a475a341d4001641cfc86d92e0166
sebi <gore.sebyx@yahoo.com>
2018-09-18 09:36:43 +0300

    update index

---
+++ frontend/src/components/page/nav-bar.vue
@@ -0,0 +1,14 @@
+<template>
+    <nav class="navbar sticky-top navbar-light bg-light">
+        <router-link :to="{ name: 'home'}" class="navbar-brand">todo APP</router-link>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: 'page-nav-bar',
+    components: {
+
+    }
+}
+</script>
