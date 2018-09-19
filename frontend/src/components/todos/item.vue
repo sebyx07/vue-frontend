62e7e44d38b3313d8cbc627c24919c055cd265ed
sebi <gore.sebyx@yahoo.com>
2018-09-19 07:35:29 +0300

    improve babel.config

---
+++ frontend/src/components/todos/item.vue
@@ -0,0 +1,23 @@
+<template>
+  <li class="list-group-item">
+    {{todo.title}}
+
+    <router-link :to="{ name: 'todo-show', params: { id:  todo.id } }">Edit</router-link>
+  </li>
+</template>
+
+<script>
+  export default {
+    name: 'todos-item',
+    components: {
+
+    },
+    props: {
+      todo: Object
+    },
+
+    data () {
+      return { }
+    },
+  }
+</script>
