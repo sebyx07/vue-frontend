b3af00e0b403e0b24540ce894f8cf4bac6b781e3
sebi <gore.sebyx@yahoo.com>
2018-09-21 02:35:07 +0300

    fix .gitignore

---
+++ frontend/src/components/todos/list.vue
@@ -0,0 +1,27 @@
+<template>
+  <ul class="list-group">
+    <todos-item v-for="todo in todos" :key="todo.id" v-bind:todo="todo"></todos-item>
+  </ul>
+</template>
+
+<script>
+  import TodosStore from "../../todo-store";
+  import TodosItem from "./item";
+  export default {
+    name: 'todos-list',
+    components: {
+      TodosItem
+
+    },
+
+    created() {
+      TodosStore.dispatch("fetchTodos")
+    },
+
+    computed: {
+      todos(){
+        return TodosStore.state.todos;
+      }
+    }
+  }
+</script>
