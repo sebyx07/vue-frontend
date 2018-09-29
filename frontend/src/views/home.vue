8f4e72db030e0f2685759c53e518bca845a3375a
sebi <gore.sebyx@yahoo.com>
2018-09-29 03:32:32 +0300

    improve babel.config

---
+++ frontend/src/views/home.vue
@@ -0,0 +1,13 @@
+<template>
+  <TodosList></TodosList>
+</template>
+
+<script>
+  import TodosList from "../components/todos/list";
+  export default {
+    name: 'home',
+    components: {
+      TodosList
+    }
+  }
+</script>
