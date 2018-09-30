e91e848630c4700cf1057f333df41877cbdfa150
sebi <gore.sebyx@yahoo.com>
2018-09-30 09:48:29 +0300

    change README

---
+++ frontend/src/views/todo-show.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Show</h1>
+</template>
+
+
+<script>
+  import TodoStore from "../todo-store";
+  export default {
+    name: 'todo-show',
+    computed: {
+      todo(){
+        return TodoStore.state.todos
+      }
+    }
+  }
+</script>
